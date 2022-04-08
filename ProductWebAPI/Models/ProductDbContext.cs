e4ddedcb3c348a02a2cccfebe853bec20bd4397c
Admin <Admin@LAPTOP-61TRJDO1>
2022-04-08 02:01:58 +0200

    Deleted default files created along with the project, added models Product.cs, ProductDbContext.cs and controller ProductController

---
+++ ProductWebAPI/Models/ProductDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductWebAPI.Models
+{
+    public class ProductDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; } = null!;
+
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+        }
+
+        public List<Product> GetProducts() => Products.Local.ToList<Product>();
+    }
+}
